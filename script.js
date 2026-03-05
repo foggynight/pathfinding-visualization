a7bd22e6efe130431db908857ce27056be114f1d
Robert Coffey <robertcoffey709@gmail.com>
2026-03-05 00:02:53 -0330

    add pathfinding interface

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ const CELL_H = 10;
 const GRID_W = Math.floor(CANV_W / CELL_W);
 const GRID_H = Math.floor(CANV_H / CELL_H);
 
+const TARGET_FPS = 165;
+const SLEEP_TIME = TARGET_FPS / 1000;
+
 const POINT_START = [24, 24];
 const POINT_END   = [36, 36];
 
@@ -15,9 +18,12 @@ const POINT_END   = [36, 36];
 const TILE_WATER  = 0;
 const TILE_GROUND = 1;
 const TILE_WALL   = 2;
-const TILE_POINT  = 3;
 
-const TILE_COLORS = ["blue", "green", "grey", "red"];
+const TILE_FINAL  = 3;
+const TILE_OPEN   = 4;
+const TILE_CLOSED = 5;
+
+const TILE_COLORS = ["blue", "green", "grey", "red", "#FFFF00", "#AAAA00"];
 function tile_color(val) { return TILE_COLORS[val]; }
 
 function tiles_load() {
@@ -33,11 +39,29 @@ function tiles_load() {
             tiles[y + 10][x] = TILE_WALL;
         }
     }
-    for (point of [POINT_START, POINT_END]) {
-        const [ix, iy] = point;
-        tiles[iy][ix] = TILE_POINT;
-    }
     return tiles;
+}
+
+// pathfinding -----------------------------------------------------------------
+
+const path_open = [];
+const path_closed = {};
+const path_final = [];
+
+function pathfind_BFS(point_start, point_end) {
+    function start() {
+
+    }
+
+    function step() {
+
+    }
+
+    function done() {
+        return true;
+    }
+
+    return [start, step, done];
 }
 
 // draw ------------------------------------------------------------------------
@@ -88,12 +112,42 @@ function draw_tiles(tiles) {
     }
 }
 
+function draw_points(points, tile_color) {
+    const color = TILE_COLORS[tile_color];
+    points.forEach(point => draw_tile(point[0], point[1], color));
+}
+
+function draw_path_open_closed() {
+    draw_open(path_open, TILE_OPEN);
+    draw_closed(path_closed, TILE_CLOSED);
+}
+
+function draw_path_final() {
+    draw_final(path_final, TILE_FINAL);
+}
+
 // main ------------------------------------------------------------------------
 
-function main() {
+async function main() {
     const tiles = tiles_load();
-    draw_clear("grey");
     draw_tiles(tiles)
+    draw_points([POINT_START, POINT_END], TILE_FINAL);
+
+    const [start, step, done] = pathfind_BFS(POINT_START, POINT_END);
+
+    start();
+    draw_path_open_closed();
+    draw_points([POINT_START, POINT_END], TILE_FINAL);
+    while (!done()) {
+        step();
+        draw_path_open_closed();
+        draw_points([POINT_START, POINT_END], TILE_FINAL);
+        await new Promise(resolve => setTimeout(resolve, SLEEP_TIME));
+    }
+    if (path_final.length > 0) {
+        draw_path_final();
+        draw_points([POINT_START, POINT_END], TILE_FINAL);
+    }
 }
 
 main()
