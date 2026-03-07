9bfec2a511fe8ac04592ab55a91b8bc41451f4c0
Robert Coffey <robertcoffey709@gmail.com>
2026-03-06 23:32:16 -0330

    add click to set path start and end

--- script.js
+++ script.js
@@ -10,14 +10,16 @@ const GRID_H = Math.floor(CANV_H / CELL_H);
 const TARGET_FPS = 165;
 const SLEEP_TIME = TARGET_FPS / 1000;
 
-const POINT_START = [24, 24];
-const POINT_END   = [36, 36];
-
 // util ------------------------------------------------------------------------
 
 const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 function Vec2_eq(v1, v2) { return (v1[0] == v2[0]) && (v1[1] == v2[1]); }
+
+const canv_to_tile = (cx, cy) => [
+	Math.floor(cx * GRID_W / CANV_W),
+	Math.floor(cy * GRID_H / CANV_H),
+];
 
 // grid ------------------------------------------------------------------------
 
@@ -30,7 +32,18 @@ const TILE_OPEN   = 4;
 const TILE_CLOSED = 5;
 const TILE_FINAL  = 6;
 
-const TILE_COLORS = ["blue", "green", "grey", "red", "#FFFF00", "#AAAA00", "white"];
+const TILE_HOVER = 7;
+
+const TILE_COLORS = [
+	"blue",
+	"green",
+	"grey",
+	"red",
+	"#FFFF00",
+	"#AAAA00",
+	"white",
+	"purple",
+];
 function tile_color(val) { return TILE_COLORS[val]; }
 
 function tiles_load() {
@@ -51,16 +64,22 @@ function tiles_load() {
 
 // pathfinding -----------------------------------------------------------------
 
+let tiles = tiles_load();
+
+let path_open = [];
+let path_closed = Array(GRID_H).fill(0)
+	.map(_ => Array(GRID_W).fill(false));
+let path_final = [];
+
+const point_start = [0, 0];
+const point_end   = [0, 0];
+
 class Node {
 	constructor(pos, parent = null) {
 		this.pos = pos;
 		this.parent = parent;
 	}
 }
-
-let path_open = [];
-let path_closed = [];
-let path_final = [];
 
 function visit_unclosed_neighbors(tile, tile_end) {
 	function visit(x, y) {
@@ -140,6 +159,8 @@ const canvas = document.getElementById("canvas");
 [canvas.width, canvas.height] = [CANV_W, CANV_H];
 const ctx = canvas.getContext("2d");
 
+let mouse_in_canv = false;
+
 function draw_line(x1, y1, x2, y2, color) {
     ctx.strokeStyle = color;
     ctx.beginPath();
@@ -196,18 +217,34 @@ function draw_path_open_closed() {
 	}
 	path_open.forEach(tile => {
 		const [x, y] = tile.pos;
-		draw_tile(x, y, TILE_OPEN)
+		draw_tile(x, y, TILE_OPEN);
 	});
 }
 
 function draw_path_final() {
 	path_final.forEach(tile => {
 		const [x, y] = tile.pos;
-		draw_tile(x, y, TILE_FINAL)
+		draw_tile(x, y, TILE_FINAL);
 	});
 }
 
+function draw_running(tiles, start_end) {
+	draw_tiles(tiles);
+	draw_path_open_closed();
+	draw_path_final();
+	draw_tile_arr(start_end, TILE_POINT);
+}
+
+function draw_hover(canvas_pos) {
+	if (!mouse_in_canv) return;
+	const [tx, ty] = canv_to_tile(...canvas_pos);
+	draw_tile(tx, ty, TILE_HOVER);
+}
+
 // main ------------------------------------------------------------------------
+
+const start_end = [point_start, point_end];
+let mouse_pos = [CANV_W / 2, CANV_H / 2];
 
 function str_to_algorithm(str) {
 	switch (str) {
@@ -221,59 +258,81 @@ function str_to_algorithm(str) {
 
 let pathfind_func = str_to_algorithm(
 	document.getElementById("select_algorithm").value
-)
-const start_end = [POINT_START, POINT_END];
+);
 
 let running = false;
 let run_die = false;
 async function run() {
 	running = true;
-	const [start, step, done] = pathfind_func(POINT_START, POINT_END);
+	const [start, step, done] = pathfind_func(point_start, point_end);
 
 	path_open = [];
 	path_closed = [];
 	path_final = [];
 
 	start();
-	draw_path_open_closed();
-	draw_tile_arr(start_end, TILE_POINT);
-
+	draw_running(tiles, start_end);
+	draw_hover(mouse_pos);
 	while (!run_die && !done()) {
 		step();
-		draw_tiles(tiles);
-		draw_path_open_closed();
-		draw_tile_arr(start_end, TILE_POINT);
+		draw_running(tiles, start_end);
+		draw_hover(mouse_pos);
 		await sleep(SLEEP_TIME);
 	}
 
 	if (path_final.length > 0) {
-		draw_path_final();
-		draw_tile_arr(start_end, TILE_POINT);
+		draw_running(tiles, start_end);
+		draw_hover(mouse_pos);
 	}
 
 	if (run_die) run_die = false;
 	running = false;
 }
 
-const tiles = tiles_load();
-draw_tiles(tiles)
-draw_tile_arr(start_end, TILE_POINT);
+function main() {
+	draw_tiles(tiles);
+	draw_tile_arr(start_end, TILE_POINT);
+	draw_hover(mouse_pos);
+}
+main()
 
 // DOM -------------------------------------------------------------------------
 
 function onchange_algorithm(value) {
 	pathfind_func = str_to_algorithm(
 		document.getElementById("select_algorithm").value
-	)
-	console.log(pathfind_func);
+	);
 }
 
-function onclick_canvas() {
-
+function get_mouse_pos(canvas, evt) {
+	const rect = canvas.getBoundingClientRect();
+	return [evt.clientX - rect.left, evt.clientY - rect.top];
 }
+canvas.addEventListener("mousemove", evt => {
+	mouse_pos = get_mouse_pos(canvas, evt);
+	draw_running(tiles, start_end);
+	draw_hover(mouse_pos);
+});
+canvas.addEventListener("mouseenter", _ => { mouse_in_canv = true; });
+canvas.addEventListener("mouseleave", _ => { mouse_in_canv = false; });
+
+let click_toggle = false;
+function onclick_canvas(_) {
+	if (running) return;
+	const [mtx, mty] = canv_to_tile(...mouse_pos);
+	if (!click_toggle) {
+		point_start[0] = mtx;
+		point_start[1] = mty;
+	} else {
+		point_end[0] = mtx;
+		point_end[1] = mty;
+	}
+	click_toggle = !click_toggle;
+}
+canvas.addEventListener("click", onclick_canvas);
 
 async function onclick_run() {
 	if (running) run_die = true;
-	while (run_die) await sleep(SLEEP_TIME);
+	while (run_die) await sleep(8 * SLEEP_TIME);
 	run();
 }
